9cfbae10087e12207d1612151ea2caecbeed6606
kengangenkay <94963544+kengangenkay@users.noreply.github.com>
2022-06-26 17:00:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+const menu = document.querySelector('.nav__menu');
+
+
+// open nav menu 
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+})
+
+
+// close nav menu 
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav);
+
+// close nav menu when menu items are clicked
+
+if(window.innerWidth < 1024) {
+    document.querySelectorAll('.nav__menu li a').forEach(navItem => {
+        navItem.addEventListener('click', closeNav)
+    })
+}
+
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+// fetches testimonials from testimonials array
+
+const fetchTestimonials = () => {
+    let testimonialsContainer = document.querySelector('.testimonials__container')
+    testimonialsContainer.innerHTML = '';
+
+    // loop through array and print testimonials
+
+    testimonials.forEach(testimonial => {
+        const newTestimonial = document.createElement('article');
+        newTestimonial.className = 'testimonial';
+        newTestimonial.innerHTML `
+        <p>
+        '${testimonial.quote}'
+      </p>
+      <div class="testimonial_client">
+          <span class="avatar">
+              <img src="./images/${testimonial.avatar}.jpg">
+          </span>
+          <div class="testimonial__work">
+              <p><b>${testimonial.name}</b></p>
+              <small>${testimonial.title}</small>
+          </div>
+      </div>
+                                `
+    })
+    testimonialsContainer.append(newTestimonial);
+}
+
+// show/hide skills items
+const skillItems = document.querySelectorAll('section.skills .skill');
+
+skillItems.forEach(skill => {
+  skill.querySelector('.head').addEventListener('click', () => {
+    skill.querySelector('.items').classList.toggle('show-items');
+  })
+})
+
+window.addEventListener('load', fetchTestimonials)
